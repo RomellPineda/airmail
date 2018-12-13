d3abe592d176eca87e9c65e9f9e65f02202eaea2
RomellPineda <romell.pineda@gmail.com>
2018-12-13 01:37:23 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/',  (req, res) => {
+    res.send({ hi: 'aloha!'});
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(5000);
